5370a3a1bb12ac52b8dd7599937d2dce63313e7b
Julio Cesar Dias Dos Santos <juliozulu2008@gmail.com>
2023-10-01 21:04:34 -0300

    Curso de TailwindCss

---
+++ projeto/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], //esta acesando a pasra src e procurando arquivos HTML e JS
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
